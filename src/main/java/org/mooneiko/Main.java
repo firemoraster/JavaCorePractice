8e664ef2fe2a65344cc71fcedc865da3a814f9de
mooneiko <moon@gmail.com+>
2025-01-29 22:28:31 +0200

    start

--- src/main/java/org/mooneiko/Main.java
+++ src/main/java/org/mooneiko/Main.java
@@ -2,6 +2,12 @@ package org.mooneiko;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); // simple println
+        System.out.print("it's really good!\n"); // print with \n as new line
+        /*
+        System.out.print("Let's create a new world together!");
+        multi-line comment
+         */
+        System.out.print("it's really good!");
     }
 }
