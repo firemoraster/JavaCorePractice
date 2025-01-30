198ef41f1aa1969ea5d5fca4a895eff1ed75f715
mooneiko <moon@gmail.com+>
2025-01-30 22:27:44 +0200

    variables

--- src/main/java/org/mooneiko/Main.java
+++ src/main/java/org/mooneiko/Main.java
@@ -1,7 +1,9 @@
 package org.mooneiko;
 
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Hello, World!"); // simple println
         System.out.print("it's really good!\n"); // print with \n as new line
         /*
@@ -10,42 +12,19 @@ public class Main {
          */
         System.out.println("it's really good!");
 
+        Variables variables = new Variables();
 
-        // integer variable  - simple one line comment
+        System.out.println("The value is " + variables.value);
+        System.out.println("$" + variables.price);
+        System.out.println("The grade is " + variables.grade);
+        System.out.println(variables.isStudent + "\n" + variables.forSale + "\n" + variables.isOnline);
 
-        //declaration
-        int quantity = 21;
-        int value = 100;
-        System.out.println("The value is "+ value);
-
-        // double variable
-        double price = 19.99;
-        double gpa = 3.5;
-        double temperatur = -2.3;
-
-        System.out.println("$"+price);
-
-        // char variable
-
-        char grade = 'A';
-        char symbol = '!';
-        char currency = '$';
-        System.out.println("The grade is "+ grade);
-
-        // boolean variable
-
-        boolean isStudent = true;
-        boolean forSale = false;
-        boolean isOnline = true;
-
-        System.out.println(isStudent +"\n"+forSale +"\n"+isOnline);
-
-        // condition statement
-
-        if(isStudent)
-        {System.out.println("You are a Student");}
-        else {
+        if (variables.isStudent) {
+            System.out.println("You are a Student");
+        } else {
             System.out.println("You are not a Student");
+
         }
+
     }
 }
---
+++ src/main/java/org/mooneiko/Variables.java
@@ -0,0 +1,36 @@
+package org.mooneiko;
+
+public class Variables {
+
+
+    // integer variable  - simple one line comment
+
+    //declaration
+    int quantity = 21;
+    int value = 100;
+
+
+    // double variable
+    double price = 19.99;
+    double gpa = 3.5;
+    double temperatur = -2.3;
+
+
+    // char variable
+
+    char grade = 'A';
+    char symbol = '!';
+    char currency = '$';
+
+
+    // boolean variable
+
+    boolean isStudent = true;
+    boolean forSale = false;
+    boolean isOnline = true;
+
+
+    // condition statement
+
+
+}
