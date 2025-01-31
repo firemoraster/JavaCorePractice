d0608823813923f57bf84f92826c021135bfc9fb
mooneiko <moon@gmail.com+>
2025-01-31 19:24:39 +0200

    using boolean and if statement

--- src/main/java/org/mooneiko/Main.java
+++ src/main/java/org/mooneiko/Main.java
@@ -1,17 +1,41 @@
 package org.mooneiko;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner scString = new Scanner(System.in);
+        Scanner scInt = new Scanner(System.in);
+
 
         System.out.print("Enter your name: ");
-        String name = sc.next();
+        String name = scString.next();
 
         System.out.println("Hello "+ name);
 
-        sc.close();
+        System.out.print("Enter your age:");
+        String age = scInt.next();
+
+        System.out.println("Your age is:"+age);
+
+        System.out.println("Your name is "+name+" and age is "+age);
+
+        System.out.print("Age you a human? (true/false)");
+        boolean isHuman = scInt.nextBoolean();
+        if (isHuman) {
+            System.out.println("You are a human");
+            System.out.println("Your name is "+name+" and age is "+age+" you are a human"+ isHuman);
+
+        }
+        else {
+            System.out.println("You are not a human");
+            System.out.println("Your name is "+name+" and age is "+age+" you are not a human"+ isHuman);
+
+        }
+
+        scString.close();
+        scInt.close();
     }
 }
