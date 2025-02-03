7159c218c0f44a4e34d1cfa7676c8e333b2e3fe6
mooneiko <moon@gmail.com+>
2025-02-03 20:31:39 +0200

    if statements added

---
+++ src/main/java/org/mooneiko/Statements.java
@@ -0,0 +1,60 @@
+package org.mooneiko;
+
+import java.util.Scanner;
+
+public class Statements {
+
+    /*
+    // if statement == performs a block of code if its condition is true
+
+    Scanner sc = new Scanner(System.in);
+
+    String name;
+    int age;
+    boolean isStudent;
+
+        System.out.print("Enter your name: ");
+    name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+    age = sc.nextInt();
+
+        System.out.print("Are you a student? ");
+    isStudent = sc.nextBoolean();
+
+    //Group 1
+        if (name.isEmpty()) {
+        System.out.println("Name is empty");
+    } else {
+        System.out.println("Name is " + name);
+    }
+
+
+    //Group 2
+        if (age >= 65) {
+        System.out.println("You are a senior!");
+
+    } else if (age >= 18) {
+        System.out.println("You are an adult!");
+    } else if (age < 0) {
+        System.out.println("You haven't born yet!");
+
+    } else if (age == 0) {
+        System.out.println("You are a baby!");
+
+    } else {
+        System.out.println("You are a child!");
+    }
+
+    //Group 3
+
+        if (isStudent) {
+        System.out.println("You are a student!");
+    } else {
+        System.out.println("You are not a student!");
+    }
+
+        sc.close();
+        S
+     */
+}
