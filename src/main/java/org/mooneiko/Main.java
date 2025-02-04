7069709828c809f641118949388f0becb0c46bc8
mooneiko <moon@gmail.com+>
2025-02-04 18:47:19 +0200

    math class added

--- src/main/java/org/mooneiko/Main.java
+++ src/main/java/org/mooneiko/Main.java
@@ -1,12 +1,11 @@
 package org.mooneiko;
+
 import java.util.Scanner;
 import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-
-
-
 
 
     }
---
+++ src/main/java/org/mooneiko/MathClass.java
@@ -0,0 +1,69 @@
+package org.mooneiko;
+
+public class MathClass {
+
+    /*
+
+        System.out.println(Math.PI);
+        System.out.print(Math.E);
+        Scanner sc = new Scanner(System.in);
+
+        double result;
+        double a;
+        double b;
+        double c;
+
+        result = Math.pow(2, 3);
+        System.out.println(result);
+        result = Math.abs(-5);
+        System.out.println(result);
+        result = Math.sqrt(9);
+        System.out.println(result);
+        result = Math.round(3.14);
+        System.out.println(result);
+        result = Math.ceil(3.14);
+        System.out.println(result);
+        result = Math.floor(3.99);
+        System.out.println(result);
+        result = Math.max(10, 20);
+        System.out.println(result);
+        result = Math.min(10, 20);
+        System.out.println(result);
+
+
+        //Hypotenuse c = Math.sqrt ( a^2+b^2)
+
+        System.out.print("Enter the length of side A:");
+        a = sc.nextDouble();
+        System.out.print("Enter the length of side B:");
+        b = sc.nextDouble();
+
+        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        System.out.println("Hypotenuse of " + a + " and " + b + " is " + c);
+
+
+        // circumference = 2 * Math.PI * radius
+
+        // area = Math.PI * Math.pow(radius,2)
+
+        // volume = (4.0/3.0)*Math.PI*Math.pow(radius,3)
+
+        double radius;
+        double circumference;
+        double area;
+        double volume;
+
+        System.out.print("Enter the radius: ");
+        radius = sc.nextDouble();
+        circumference = 2 * radius * Math.PI;
+        System.out.printf("Circumference of " + radius + " is %.1fcm\n", circumference);
+
+        area = Math.PI * Math.pow(radius,2);
+        System.out.printf("Area of " + radius + " is %.1fcm²\n", area);
+        volume = (4.0/3.0)*Math.PI*Math.pow(radius,3);
+        System.out.printf("Volume of " + radius + " is %.1fcm³", volume );
+
+        sc.close();
+
+     */
+}
