9d27ade9a00ec49d6218092c514cce6911343cb7
mooneiko <moon@gmail.com+>
2025-02-05 20:04:31 +0200

    printf added

---
+++ src/main/java/org/mooneiko/Printf.java
@@ -0,0 +1,76 @@
+package org.mooneiko;
+
+public class Printf {
+    /*
+     // printf() = is a method used to format output
+
+        // % [flags][width][.precision][specifier-character]
+
+
+        String name = "Spongebob";
+
+        char firstLetter = 'S';
+        int age = 30;
+        double height = 60.5;
+
+        boolean isEmployed = true;
+
+        System.out.printf("Hello %s\n", name);
+        System.out.printf("Your name starts with a %c\n", firstLetter);
+        System.out.printf("Your age is %d\n", age);
+        System.out.printf("Your height is %.2f\n", height);
+        System.out.printf("Your isEmployed is %b\n", isEmployed);
+
+        System.out.printf("%s is %d years old\n", name, age);
+
+        double price1 = 9.99;
+        double price2 = 100.15;
+        double price3 = -54.01;
+
+        // [flags]
+        // + = output a plus
+        // , = comma grouping separator
+        // ( = negative numbers are enclosed in ()
+        // space = display a minus if  negative, space if positive
+        System.out.println("+");
+        System.out.printf("%+.2f\n", price1);
+        System.out.printf("%+.2f\n", price2);
+        System.out.printf("%+.2f\n", price3);
+
+        System.out.println(" space");
+        System.out.printf("% .2f\n", price1);
+        System.out.printf("% .2f\n", price2);
+        System.out.printf("% .2f\n", price3);
+
+
+        price1 = 9000.99;
+        price2 = 100000.15;
+        price3 = -54000.01;
+        System.out.println(",");
+        System.out.printf("%,.2f\n", price1);
+        System.out.printf("%,.2f\n", price2);
+        System.out.printf("%,.2f\n", price3);
+
+        int id1 =1;
+        int id2 = 23;
+        int id3 = 456;
+        int id4 = 7890;
+
+        // zero padding
+        System.out.printf("%04d\n", id1);
+        System.out.printf("%04d\n", id2);
+        System.out.printf("%04d\n", id3);
+        System.out.printf("%04d\n", id4);
+
+        //spaces
+        System.out.printf("%4d\n", id1);
+        System.out.printf("%4d\n", id2);
+        System.out.printf("%4d\n", id3);
+        System.out.printf("%4d\n", id4);
+
+        System.out.printf("%-4d\n", id1);
+        System.out.printf("%-4d\n", id2);
+        System.out.printf("%-4d\n", id3);
+        System.out.printf("%-4d\n", id4);
+     */
+}
